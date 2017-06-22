aac8b386b1f1b79b3668cbe28a03b15f5587c2d0
CDX <cdxofemail@163.com>
2017-06-22 15:08:15 +0800

    vue-music init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hahaha
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang='stylus' rel="stylesheet" scoped>
+  @import "common/stylus/variable.styl"
+
+  #app
+    color $color-theme
+</style>
