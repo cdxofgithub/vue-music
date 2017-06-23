4a1011914b045e09134dc013d929cb4c148692b9
CDX <cdxofemail@163.com>
2017-06-23 20:32:35 +0800

    update slider 组件

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot></slot>
+    </div>
+    <div class="dots">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
