87b5aee800cd4b25964f1845d2224517741802f7
cdxofgithub <cdxofemail@163.com>
2017-08-10 15:58:31 +0800

    update singer

--- src/components/singer-datail/singer-detail.vue
+++ src/components/singer-datail/singer-detail.vue
@@ -5,22 +5,53 @@
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
+import { getSingerDetail } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import {createSong} from 'common/js/song'
 
+export default {
+  computed: {
+    ...mapGetters([
+      'singer'
+    ])
+  },
+  data() {
+    return {
+      songs: []
+    }
+  },
+  created() {
+    this._getDetail()
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push('/singer')
+      }
+      getSingerDetail(this.singer.id).then((res) => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      })
+    },
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach((item) => {
+        let { musicData } = item
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
+      return ret
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import "~common/stylus/variable"
-
-  .singer-detail
-    position fixed
-    z-index 100
-    top 0
-    right 0
-    left 0
-    bottom 0
-    background $color-background
 
   .slide-enter-active, .slide-leave-active
     transition all 0.3s
